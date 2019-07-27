65f93f959e6155f12f8e8c54fd1de8e18a39ff47
Diego Santos Luiz <diego.santos.luiz@gmail.com>
2019-07-27 09:51:17 -0300

    Aula 02 - Atividade 05: Encapsulando as rotas

--- casadocodigo/server.js
+++ casadocodigo/server.js
@@ -3,27 +3,3 @@ const app = require('./src/config/custom-express');
 app.listen(3000, function() {
   console.log(`Servidor rodando na posta 3000`);
 });
-
-app.get('/', function(req, resp) {
-  resp.send(
-    `<html>
-      <head>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1>Casa do Código</h1>
-      </body> 
-    </html>`);   
-});
-
-app.get('/livros', function(req, resp) {
-  resp.send(
-    `<html>
-      <head>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1>Listagem de livros</h1>
-      </body> 
-    </html>`);   
-});
