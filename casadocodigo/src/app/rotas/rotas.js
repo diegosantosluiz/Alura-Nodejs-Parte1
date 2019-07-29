011e3f7871a885d6e51311bc4cdec43d656f7ab8
Diego Santos Luiz <diego.santos.luiz@gmail.com>
2019-07-29 18:33:42 -0300

    Aula 03 - Atividade 04: O padrão DAO

--- casadocodigo/src/app/rotas/rotas.js
+++ casadocodigo/src/app/rotas/rotas.js
@@ -1,3 +1,4 @@
+const LivroDao = require('../infra/livro-dao');
 const db = require('../../config/database');
 
 module.exports = (app) => {
@@ -14,7 +15,9 @@ module.exports = (app) => {
   });
   
   app.get('/livros', function(req, resp) {
-    db.all('SELECT * FROM LIVROS', function(erro, resultados) {
+    
+    const livroDao = new LivroDao(db);
+    livroDao.lista(function(erro, resultados) {
       resp.marko(
         require('../views/livros/lista/lista.marko'),
         {
