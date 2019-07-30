1bfe94ccee254e9918d11068867708d7722d0ca4
Diego Santos Luiz <diego.santos.luiz@gmail.com>
2019-07-29 22:07:17 -0300

    Aula 03 - Atividade 05: DAO com Promises

--- casadocodigo/src/app/infra/livro-dao.js
+++ casadocodigo/src/app/infra/livro-dao.js
@@ -3,12 +3,19 @@ class LivroDao {
     this._db = db;
   }
 
-  lista(callback) {
-    this._db.all(
-      'SELECT * FROM LIVROS',
-      (erro, resultados) => 
-        callback(erro, resultados)
-    );
+  lista() {
+    return new Promise((resolve, reject) => {
+      this._db.all(
+        'SELECT * FROM LIVROS',
+        (erro, resultados) => {
+          if (erro)
+            return reject('Não foi possível listar os livros!');
+
+          return resolve(resultados);
+        }
+      );
+    });
+    
   }
 }
 
