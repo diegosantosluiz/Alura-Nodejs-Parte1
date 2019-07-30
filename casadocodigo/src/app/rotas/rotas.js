d5e1a8e18c623541f190a8175814c943523a601b
Diego Santos Luiz <diego.santos.luiz@gmail.com>
2019-07-30 14:00:37 -0300

    Aula 05 - Atividade 06: Edição de livros

--- casadocodigo/src/app/rotas/rotas.js
+++ casadocodigo/src/app/rotas/rotas.js
@@ -29,6 +29,26 @@ module.exports = (app) => {
   });
 
   app.get('/livros/form', function(req, resp) {
+    resp.marko(
+      require('../views/livros/form/form.marko'), 
+      { livro: {} }
+    );
+  });
+
+  app.get('/livros/form/:id', function(req, resp) {
+    const id = req.params.id;
+
+    const livroDao = new LivroDao(db);
+    livroDao.buscaPorId(id)
+      .then(livro =>
+        resp.marko(
+          require('../views/livros/form/form.marko'), 
+          {
+            livro: livro
+          }
+      ))
+      .catch(erro => console.log(erro));
+    
     resp.marko(require('../views/livros/form/form.marko'));
   });
 
